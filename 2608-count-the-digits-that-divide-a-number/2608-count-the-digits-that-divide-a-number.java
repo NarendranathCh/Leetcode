3ef002669ac1c5e9088025055873b1be6769528a
NarendranathCh <149961832+NarendranathCh@users.noreply.github.com>
2025-07-22 11:46:13 +0530

    Time: 0 ms (100%), Space: 40.7 MB (28.43%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.java
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.java
@@ -1,14 +1,17 @@
 class Solution {
     public int countDigits(int num) {
-       int ans=0;
-       int n=num;
-       while(n>0){
-        int x = n%10;
-        if(x!=0 && num%x==0){
-            ans++;
+        int count = 0;
+        int original = num;
+
+        while (num > 0) {
+            int digit = num % 10;
+            
+            if (digit != 0 && original % digit == 0) {
+                count++;
+            }
+            num /= 10;
         }
-        n=n/10;
-       } 
-    return ans;
+
+        return count;
     }
 }
